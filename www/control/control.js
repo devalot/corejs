e99be51b2f1b05b0b886bd7e8844fedc919f0ad2
Peter Jones <pjones@pmade.com>
2015-08-05 11:02:53 -0600

    Add control flow exercise

---
+++ www/control/control.js
@@ -0,0 +1,55 @@
+// Leave the next line intact and ignore it for now.
+ControlFlow = function(test) {
+
+  /****************************************************************************/
+  // EXERCISE 1:
+  //
+  // In the anonymous function below, return true if the input
+  // variable is equal to the number 42.  Otherwise return false.  Use
+  // an `if' statement to achieve this.
+  test.confirm(function(input) { // Write your code below this line.
+
+
+  }); // End of the anonymous function.
+
+  /****************************************************************************/
+  // EXERCISE 2:
+  //
+  // In the anonymous function below, return true if the input
+  // variable is equal to the number 42.  If the input is the number
+  // 43 then return null.  In all other cases return false.
+  //
+  // Use an `if' statement to achieve this.
+  test.confirm(function(input) { // Write your code below this line.
+
+
+  }); // End of the anonymous function.
+
+  /****************************************************************************/
+  // EXERCISE 3:
+  //
+  // Repeat exercise 2, this time using a `switch' statement.
+  test.confirm(function(input) { // Write your code below this line.
+
+
+  }); // End of the anonymous function.
+
+  /****************************************************************************/
+  // EXERCISE 4:
+  //
+  // In the anonymous function below, write a loop that calls the
+  // `counter' function exactly 3 times.
+  //
+  // Example of calling the counter function:
+  //
+  //     counter();
+  //
+  test.confirm(function(counter) { // Write your code below this line.
+
+
+  }); // End of the anonymous function.
+
+
+
+  // Don't change anything below this line.
+};
---
+++ www/lib/test/control_test.js
@@ -0,0 +1,48 @@
+$(function() {
+
+  var e2And3 = function(value, test) {
+    test.assertInstanceOf(value, Function);
+    test.assert(value(42)   === true,  "should have returned true when given 42");
+    test.assert(value(1)    === false, "should have returned false when given 1");
+    test.assert(value("42") === false, "should have returned false when given string \"42\"");
+    test.assert(value(43)   === null,  "should have returned null when given 43");
+    test.assert(value("43") === false, "should have returned false when given string \"43\"");
+    return true;
+  };
+
+  var checks = [
+
+    /**************************************************************************/
+    // Exercise 1:
+    function(value, test) {
+      test.assertInstanceOf(value, Function);
+      test.assert(value(42)   === true,  "should have returned true when given 42");
+      test.assert(value(1)    === false, "should have returned false when given 1");
+      test.assert(value("42") === false, "should have returned false when given string \"42\"");
+      return true;
+    },
+
+    /**************************************************************************/
+    // Exercise 2 and 3:
+    e2And3,
+    e2And3,
+
+    /**************************************************************************/
+    // Exercise 4:
+    function(value, test) {
+      test.assertInstanceOf(value, Function);
+
+      var times = 0;
+      var counter = function() {times += 1;};
+
+      value(counter);
+      test.assert(times === 3, "the counter function should have been called 3 times");
+      return true;
+    },
+
+  ];
+
+  var confirm = new Confirm();
+  var control = ControlFlow(confirm);
+  confirm.assertValues(checks);
+});
