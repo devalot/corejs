7597c52efaab22dff7adbe775fb78452887ffbd1
Peter Jones <pjones@pmade.com>
2015-08-12 13:51:25 -0600

    Add the flags exercise

---
+++ www/flags/flags.js
@@ -0,0 +1,31 @@
+/*
+ * Using only the built-in DOM methods, find the elements that have
+ * the text "FLAG" inside them and move them (the element) into the
+ * div with id of #bucket.
+ *
+ * Hint: you can use:
+ *
+ *   - document.getElementById
+ *   - document.getElementsByTagName
+ *   - document.getElementsByClassName
+ *   - document.querySelector
+ *   - document.querySlectorAll
+ *
+ * and all the parent, child and sibling selectors, along with the DOM
+ * traversal properties such as:
+ *
+ *   - parentNode
+ *   - previousSibling
+ *   - nextSibling
+ *   - firstChild
+ *   - lastChild
+ *   - childNodes
+ *
+ * Make sure to read the index.html file and study the locations of
+ * the FLAG elements.
+ */
+(function() { // Keep this line.
+
+  // Your code here.
+
+}); // Keep this line.
