820994adc723e9e2169f9ba9a423970f68c8e8cb
Peter Jones <pjones@pmade.com>
2015-09-22 21:03:52 -0600

    Small changes to the closure test and main index

--- www/lib/test/closure_test.js
+++ www/lib/test/closure_test.js
@@ -7,6 +7,7 @@ $(function() {
       test.assertInstanceOf(api, Object);
       test.assert(api.getTemp, "should have a getTemp method");
       test.assert(api.setTemp, "should have a setTemp method");
+      test.assert(Object.keys(api).length === 2, "should only have two properties");
 
       var now = Date.now();
       api.setTemp(now);
